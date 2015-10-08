6991a52e6307a7137b68477988510ca8e25ed2cf
鈴木 千絵 <suzukichie@suzuki-no-MacBook-Pro.local>
2015-10-08 16:48:34 +0900

    Clean files

--- app/src/Controller/UsersController.php
+++ app/src/Controller/UsersController.php
@@ -20,6 +20,17 @@ class UsersController extends AppController
     }
 
     /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $this->set('users', $this->paginate($this->Users));
+        $this->set('_serialize', ['users']);
+    }
+
+    /**
      * View method
      *
      * @param string|null $id User id.
@@ -33,12 +44,6 @@ class UsersController extends AppController
         ]);
         $this->set('user', $user);
         $this->set('_serialize', ['user']);
-
-        if ((int)$this->Auth->user('id') === (int)$id) {
-            $this->set('is_self_page', true);
-        } else {
-            $this->set('is_self_page', false);
-        }
     }
 
     /**
@@ -52,20 +57,62 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->data);
             $user->set('password', User::hashPassword($user->get('password')));
+            // $user = $this->Users->patchEntity($sex, $this->request->data);
             $user->set('created_at', time());
             $user->set('updated_at', time());
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect([
-                    'controller' => 'Pages',
-                    'action' => 'display',
-                ]);
+                return $this->redirect(['action' => 'login']);
             } else {
                 $this->Flash->error(__('The user could not be saved. Please, try again.'));
             }
         }
         $this->set(compact('user'));
         $this->set('_serialize', ['user']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Network\Response|void
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $user = $this->Users->patchEntity($user, $this->request->data);
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            } else {
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
+        }
+        $this->set(compact('user'));
+        $this->set('_serialize', ['user']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id User id.
+     * @return \Cake\Network\Response|void
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $user = $this->Users->get($id);
+        if ($this->Users->delete($user)) {
+            $this->Flash->success(__('The user has been deleted.'));
+        } else {
+            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(['action' => 'index']);
     }
 
     public function signup()
