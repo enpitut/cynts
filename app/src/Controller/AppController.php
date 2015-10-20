b5046a1bd36e1078ebf4572e47a051df058ec724
tasuwo <kamuhata.you@gmail.com>
2015-10-20 17:31:22 +0900

    Fix bug

--- app/src/Controller/AppController.php
+++ app/src/Controller/AppController.php
@@ -54,7 +54,7 @@ class AppController extends Controller
     {
         parent::initialize();
         $this->loadComponent('Flash');
-        $this->autoLayout = false;
+        $this->viewBuilder()->autoLayout(false);
     }
 
     public function isAuthorized($user = null)
