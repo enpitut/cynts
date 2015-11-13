0e489a6c85804608c121369c501281f3407cdc88
metalunk <chiba.r221@gmail.com>
2015-11-13 15:14:43 +0900

    Remove unused error messages

--- app/src/Controller/UsersController.php
+++ app/src/Controller/UsersController.php
@@ -53,10 +53,7 @@ class UsersController extends AppController
             $user->set('created_at', time());
             $user->set('updated_at', time());
             if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
                 return $this->redirect(['action' => 'login']);
-            } else {
-                $this->Flash->error(__('The user could not be saved. Please, try again.'));
             }
         }
         $this->set('user', $user);
