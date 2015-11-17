b2806cb3e8ce54fcd5a1e03cb04b9a3739478f53
tasuwo <kamuhata.you@gmail.com>
2015-11-18 06:37:24 +0900

    Add accessor for getting price and sex to coordinate entity

--- app/src/Model/Entity/Coordinate.php
+++ app/src/Model/Entity/Coordinate.php
@@ -2,6 +2,7 @@
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
 
 /**
  * Coordinate Entity.
@@ -30,5 +31,43 @@ class Coordinate extends Entity
     protected function _getPhotoPath()
     {
         return self::COORDINATE_PHOTO_DIRECTORY_ROOT . $this->_properties['photo'];
+    }
+
+    protected function _getPrice()
+    {
+        // もっとうまい持ってき方があるかも...?
+        $coordinate_id = $this->_properties['id'];
+        $coordinate_table = TableRegistry::get('Coordinates');
+        $coordinate = $coordinate_table->get($coordinate_id, [
+                'contain' => ['Items']
+            ]
+        );
+
+        $coordinate_price = 0;
+        foreach($coordinate->items as $item) {
+            $coordinate_price += (int)$item->price;
+        }
+        return $coordinate_price;
+    }
+
+    /**
+     * @return int
+     */
+    protected function _getSex()
+    {
+        $coordinate_id = $this->_properties['id'];
+        $coordinate_table = TableRegistry::get('Coordinates');
+        $coordinate = $coordinate_table->get($coordinate_id, [
+                'contain' => ['Items']
+            ]
+        );
+
+        // 女性物の Item が多ければ女性，男性物が多ければ男性と判断する
+        // TODO: 本当は，投稿時に女性 or 男性を選択できるようにした方が良い
+        $sexes_weight = [];
+        foreach($coordinate->items as $item) {
+            $sexes_weight[$item->sex]++;
+        }
+        return array_search(max($sexes_weight), $sexes_weight);
     }
 }
