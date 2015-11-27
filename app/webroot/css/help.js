5e204f01b4d1bdbc0b325b1caa1b97ce7051c962
NoritsuguY <aata2815@chiba-u.jp>
2015-11-28 03:21:28 +0900

    Read json file corresponded to controller and action

--- app/webroot/css/help.js
+++ app/webroot/css/help.js
@@ -1,11 +1,14 @@
-/**
- * Created by admin on 2015/11/28.
- */
+var $script_id = $('#help_script');
+var caller_url = ($script_id.attr('controller') + "_" + $script_id.attr('action')).tolowerCase();
 
 $(window).load(function() {
-
     //定数読込
-    $.getJSON("help_data.json", function(json) {
-
+    $.getJSON("help_data/" + caller_url + ".json", function(data) {
+        for(var id in data) {
+            $("#" + id).attr({
+                'data_intro': id.data_intro,
+                'data_position': id.position
+            });
+        }
     });
 });
