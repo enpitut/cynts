2fabb622ebed2c002b17fe2430cdc1141da0708f
NoritsuguY <aata2815@chiba-u.jp>
2015-12-07 20:18:54 +0900

    Add help title and set center position

--- app/src/Template/Pages/help.ctp
+++ app/src/Template/Pages/help.ctp
@@ -1,16 +1,10 @@
 <?= $this->Html->css('help.css') ?>
 <div id="modal_help_coordinatesbattle_battle">
-    <div class="help_window_header_title">
-        タイトルクリックでトップページに戻れます
-    </div>
-
-    <div class="help_window_battlecolumn">
-        出てくる2枚の画像を見て，どっちが好きかを選ぶだけ
-    </div>
-
-    <div class="help_window_finish_button">
-        この2択ゲームを終了するときはここに出るボタンを押そう<br>
-        自分のセンスを確認できます
+    <div class="help_window">
+        このページでは，提示される2つのコーディネートから<span>自分の好きなコーディネートを選ぶゲーム</span>で遊べます<br>
+        色々なコーディネートを見て楽しみましょう！<br>
+        ゲームをやめる時には，下に表示される「ゲーム終了！」ボタンを押しましょう<br>
+        自分のセンスが<span>どの程度他のみんなと合ってるか</span>がわかります
     </div>
 </div>
 
--- app/webroot/js/help.js
+++ app/webroot/js/help.js
@@ -5,7 +5,6 @@
 const HELP_ROOT = location.origin + "/pages/"
 var $script_id = $('#help_script');
 var caller_url = ($script_id.attr('controller') + "_" + $script_id.attr('action')).toLowerCase();
-console.log(caller_url);
 
 $("#modal_open").click(
     function () {
@@ -14,25 +13,23 @@ $("#modal_open").click(
 
         $("body").append('<div id="modal_overlay"></div>');
         $("body").append('<div id="close_message">クリックすれば，この画面は閉じます．</div>');
-        $("body").append('<div id="modal_help"></div>');
-        $("#modal_help").load(HELP_ROOT + "help #modal_help_" + caller_url, function(data) {
+        $("body").append('<div id="modal_help"><div id="center_window"></div></div>');
+        $("#center_window").load(HELP_ROOT + "help #modal_help_" + caller_url, function(data) {
+            $(document).ready(function() {
+                var help_elements = $("#modal_help_" + caller_url).children();
+                jQuery.each(help_elements, function() {
+                    var class_name = this.className.replace(/help_window_/g, "");
+                    $("#modal_help_" + caller_url).prepend('<div id="help_title">ヘルプ</div>')
+                });
+                $("#modal_overlay, #modal_help, #close_message").fadeIn("slow");
 
-            var help_elements = $("#modal_help_" + caller_url).children();
-            jQuery.each(help_elements, function() {
-                var class_name = this.className.replace(/help_window_/g, "");
-
+                $("#modal_overlay, #modal_help, #close_message").unbind().click(function() {
+                    $("#modal_overlay, #modal_help, #close_message").fadeOut("slow", function() {
+                        $("#modal_overlay, #modal_help, #close_message").remove();
+                    });
+                });
             });
-            if(data === null) {
-                console.log("Reading Error");
-            }
+
         })
-
-        $("#modal_overlay, #modal_help, #close_message").fadeIn("slow");
-
-        $("#modal_overlay, #modal_help, #close_message").unbind().click(function() {
-            $("#modal_overlay, #modal_help, #close_message").fadeOut("slow", function() {
-                $("#modal_overlay, #modal_help, #close_message").remove();
-            });
-        });
     }
 )
